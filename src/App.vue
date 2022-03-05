ce3d0272c15765e5ef2696cf79ead1ae317e6645
venzel <eneas.eng@yahoo.com>
2022-03-05 10:55:03 -0300

    feat: cria um filtro computado

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
         <Cpf />
         <hr />
         <SpacesWithCommas />
+        <hr />
+        <WordLength />
     </div>
 </template>
 
 <script>
 import Cpf from './views/Cpf.vue';
 import SpacesWithCommas from './views/SpacesWithCommas.vue';
+import WordLength from './views/WordLength.vue';
 
 export default {
-    components: { Cpf, SpacesWithCommas },
+    components: { Cpf, SpacesWithCommas, WordLength },
 };
 </script>
 
---
+++ src/views/WordLength.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div>{{ word }}</div>
+        <div>{{ word | countWords }}</div>
+        <hr />
+        <div>{{ countWordsComputed }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            word: 'isso vai ficar surpreendente',
+        };
+    },
+    filters: {
+        countWords(value) {
+            return value
+                .split(' ')
+                .map((e) => `${e} (${e.length})`)
+                .join(' ');
+        },
+    },
+    computed: {
+        countWordsComputed() {
+            return this.word
+                .split(' ')
+                .map((e) => `${e} (${e.length})`)
+                .join(' ');
+        },
+    },
+};
+</script>
+
+<style></style>
